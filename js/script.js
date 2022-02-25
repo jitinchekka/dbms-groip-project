9e01d8fd3eb940ec4da60b555f8d7eff8e8528ed
jitinchekka <jitinchekka2@gmail.com>
2022-02-25 18:13:51 +0530

    joined codes

--- js/script.js
+++ js/script.js
@@ -29,3 +29,12 @@ window.onscroll = () => {
     }
   });
 };
+
+var counter = 1;
+setInterval(function() {
+	document.getElementById("r" + counter).checked = true;
+	counter++;
+	if (counter > 3) {
+		counter = 1;
+	}
+}, 5000);

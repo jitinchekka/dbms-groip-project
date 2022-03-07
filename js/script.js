c6e2bb60db8bfd2c95bba16a51d57278a56bc3f2
jitinchekka <jitinchekka2@gmail.com>
2022-03-07 10:52:30 +0530

    corrected slider time, added transition to cards

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ var counter = 1;
 setInterval(function() {
     document.getElementById("r" + counter).checked = true;
     counter++;
-    if (counter > 3) {
+    if (counter > 5) {
         counter = 1;
     }
-}, 5000);
+}, 3000);
